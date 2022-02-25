fcbd060854ac25be607ca180d516346d751f29c2
Patsu <reyesbho@gmail.com>
2022-02-25 14:32:42 -0600

    adding eureka server

---
+++ registry-service/src/test/java/f/practice/store/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package f.practice.store.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
