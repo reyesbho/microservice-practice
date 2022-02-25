da18972b532ab406d4a535e3cc7bb8a99fddc8e0
Patsu <reyesbho@gmail.com>
2022-02-25 14:56:52 -0600

    adding eureka client

--- product-service/src/main/java/f4/practice/store/StoreApplication.java
+++ product-service/src/main/java/f4/practice/store/StoreApplication.java
@@ -2,7 +2,9 @@ package f4.practice.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class StoreApplication {
 
